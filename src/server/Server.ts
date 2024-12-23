018d37f984a73599093ae3447579da8b47328f9d
hedonvictor <hedonbresciani@gmail.com>
2024-12-23 11:48:47 -0300

    added order by in getAll provider

--- src/server/Server.ts
+++ src/server/Server.ts
@@ -1,10 +1,19 @@
 import express from "express";
+import 'dotenv/config'
+import cors from 'cors'
+
 import './shared/services/translationsYup'
 import {router} from './routes'
-
+3
 const server = express();
 
 server.use(express.json());
+server.use(cors());
+
+// server.use(cors({
+//     origin: process.env.ENABLED_CORS?.split(';') || []
+// }));
+
 server.use(router);
 
 export {server};
